f0c64982ca5264990e62bc36373d522c2e527507
mariyellow <space.star.1016.f@gmail.com>
2022-12-06 21:42:22 +0900

    add php sample code (modify docker-compose, too)

---
+++ src/php/start.php
@@ -0,0 +1,29 @@
+<html>
+ <head>
+  <title>php code list</title>
+ </head>
+ <body>
+<!-- 本当はこんなHTMLよろしくないと思う。まだ勉強不足。。。Localhostだから許してください。 -->
+  <h1>php sample code for `good code / bad code`</h1>
+
+  <p>section 1</p>
+  <ul>
+    <li><a href="./section1/Main.php">Main.php</a>をみる</li>
+  </ul>
+
+  <p>section 2</p>
+  <ul>
+    <li><a href="./section2/Main.php">Main.php</a>をみる</li>
+  </ul>
+
+  <p>section 3</p>
+  <ul>
+    <li><a href="./section3/Main.php">Main.php</a>をみる</li>
+  </ul>
+
+  <p>section 4</p>
+  <ul>
+    <li><a href="./section4/Main.php">Main.php</a>をみる</li>
+  </ul>
+ </body>
+</html>
