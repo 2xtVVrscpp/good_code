44e8e65c0980deaa3896f2b2cfd3fec613c68bb6
sakaaya <space.star.1016.f@gmail.com>
2022-12-14 21:52:46 +0900

    add section2 php code

--- src/php/section2/Main.php
+++ src/php/section2/Main.php
@@ -1,2 +1,12 @@
 <?php
-    echo "Hello World!";
+require_once __DIR__ . '/HitPoint.php';
+
+
+$personAaaHitpoint = new HitPointServise\HitPoint(90);
+echo 'personAaa : ' . $personAaaHitpoint->_hitPointValue . "\n";
+
+$personAaaHitpoint = $personAaaHitpoint->calcReceveDamage(10);
+echo 'personAaa : ' . $personAaaHitpoint->_hitPointValue . "\n";
+
+$personAaaHitpoint = $personAaaHitpoint->calcRecoverHitPoint(20);
+echo 'personAaa : ' . $personAaaHitpoint->_hitPointValue . "\n";
