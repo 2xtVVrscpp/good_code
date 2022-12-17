f1e89115c0797440c8334c1f0b6be4b280a8b623
sakaaya <space.star.1016.f@gmail.com>
2022-12-17 17:32:15 +0900

    add sample5_1

---
+++ src/java/section5/sample5_1/MoneyData.java
@@ -0,0 +1,5 @@
+package section5.sample5_1;
+
+public class MoneyData {
+	public int amount;
+}
---
+++ src/java/section5/sample5_1/OrderManager.java
@@ -0,0 +1,9 @@
+package section5.sample5_1;
+
+// list 5.1 
+class OrderManager {
+	static int add(int moneyAmount1, int monerAmount2) {
+		return moneyAmount1 + monerAmount2;
+	}
+
+}
