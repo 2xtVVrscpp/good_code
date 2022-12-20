71f59405730c39937dc5a5fb6891cdc6d5e4148d
sakaaya <space.star.1016.f@gmail.com>
2022-12-20 22:24:45 +0900

    Commit what I missed save

--- src/java/section5/summary/refactorSakamoto/Main.java
+++ src/java/section5/summary/refactorSakamoto/Main.java
@@ -7,34 +7,28 @@ public class Main {
 		Logger.printInfo("start process");
 
 		// １冊目の本を定義する
-		String bookA = "goodBook";
-		int bookAPrice = 3000;
-		int bookAStored = 100;
+		BookManager bookA = new BookManager("goodBook", 3000, 100);
 
 		// ２冊目の本を定義する
-		String bookB = "badBook";
-		int bookBPrice = 6000;
-		int bookBStored = 10;
+		BookManager bookB = new BookManager("badBook", 6000, 10);
 
 		// 売上高を初期化する
 		int salesAmount = 0;
 
 		// １人目の購入後の状況を出力する
 		int buyNum1 = 2;
-		System.out.println(buyNum1 + " " + bookA + " sold out");
-		salesAmount += bookAPrice * buyNum1;
-		bookAStored -= buyNum1;
-		System.out.println(bookA + " is stored " + bookAStored);
+		bookA.afterSoldBookStored(buyNum1);
+		bookA.printBookBuyNumInfo(buyNum1);
+		salesAmount = bookA.calcSalesAmount(buyNum1, salesAmount);
 		System.out.println("Sales Amount is " + salesAmount);
 
 		System.out.println("\n");
 
 		// ２人目の購入後の状況を出力する
 		int buyNum2 = 10;
-		System.out.println(buyNum2 + " " + bookB + " sold out");
-		salesAmount += bookBPrice * buyNum2;
-		bookBStored -= buyNum2;
-		System.out.println(bookB + " is stored " + bookBStored);
+		bookA.afterSoldBookStored(buyNum2);
+		bookA.printBookBuyNumInfo(buyNum2);
+		salesAmount = bookA.calcSalesAmount(buyNum2, salesAmount);
 		System.out.println("Sales Amount is " + salesAmount);
 
 		// 終了ログを出力する
@@ -42,33 +36,3 @@ public class Main {
 
 	}
 }
-/*
- * public class StockManagement {
- * String _bookName;
- * int _bookPrice;
- * int _stockNum;
- * int _salesAmount;
- * 
- * StockManagement(String bookName, int bookPrice) {
- * if (bookName == "") {
- * System.out.println("Error : not input bookname \n");
- * System.exit(0);
- * }
- * if (bookPrice < 0) {
- * System.out.println("Error : price is more than 0 \n");
- * System.exit(0);
- * }
- * this._bookName = bookName;
- * this._bookPrice = bookPrice;
- * }
- * 
- * // 購入できるか
- * 
- * // 売上高の計算
- * // 残りの冊数
- * }
- * 
- * public class Logger {
- * 
- * }
- */
