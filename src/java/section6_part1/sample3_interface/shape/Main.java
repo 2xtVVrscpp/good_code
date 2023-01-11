db155336d3fa5cdc7a99bda1756c8b0f379d90c0
koga <m.koga201003@gmail.com>
2023-01-11 23:04:09 +0900

    add casting args comments

--- src/java/section6_part1/sample3_interface/shape/Main.java
+++ src/java/section6_part1/sample3_interface/shape/Main.java
@@ -2,18 +2,26 @@ package section6_part1.sample3_interface.shape;
 
 public class Main {
     public static void main(String[] args) {
-        // 円
-        Shape shape = new Circle(10);
-        System.out.println(shape.area());
+        // 丸
+        Circle circle = new Circle(10);
+        System.out.println(circle.area());
 
         // 四角形
-        shape = new Rectangle(20, 25);
-        System.out.println(shape.area());
+        Rectangle rectangle = new Rectangle(20, 25);
+        System.out.println(rectangle.area());
 
-        showArea(shape);
+        showArea(circle);
+        showArea(rectangle);
+
+        // showArea()の引数がアップキャストされても変数の型はそのまま
+        System.out.println(circle.getClass());
+        System.out.println(rectangle.getClass());
     }
 
-    // 図形の面積を計算
+    /**
+     * 図形の面積を計算
+     * @param shape CircleやRectangleはShapeにアップキャストされる
+     */
     static void showArea(Shape shape) {
         System.out.println(shape.area());
     }
