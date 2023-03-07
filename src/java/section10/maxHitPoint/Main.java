7c6a549e36a674dd1b70e147ada08166462e5de5
KoichiroHino <ko160424@gmail.com>
2023-03-07 22:46:15 +0900

    Memberクラスに装備変更の機能を追加

--- src/java/section10/maxHitPoint/Main.java
+++ src/java/section10/maxHitPoint/Main.java
@@ -5,8 +5,18 @@ public class Main {
 		OriginalMaxHitPoint originalMaxHitPoint = new OriginalMaxHitPoint(50);
 		Accessory accessory = new Accessory(10);
 		Armor armor = new Armor(20);
-		Member member = new Member(originalMaxHitPoint, accessory, armor);
 
+		Member member = new Member(originalMaxHitPoint, accessory, armor);
+		member.showAbility();
+
+		// 最大HPを+40させるアクセサリーに装備変更
+		Accessory accessory2 = new Accessory(40);
+		member.equip(accessory2);
+		member.showAbility();
+
+		// 最大HPを+60させる胸防具に装備変更
+		Armor armor2 = new Armor(60);
+		member.equip(armor2);
 		member.showAbility();
 	}
 }
--- src/java/section10/maxHitPoint/Member.java
+++ src/java/section10/maxHitPoint/Member.java
@@ -15,10 +15,26 @@ public class Member {
 		_correctedMaxHitPoint = new CorrectedMaxHitPoint(_originalMaxHitPoint, _accessory, _armor);
 	}
 
+	// equipAccessoryやequipArmorと名付けると動詞+目的語となる
+	// javaのオーバーロードを利用してequipメソッドを2つ作る
+	public void equip(Accessory accessory){
+		_accessory = accessory;
+		// 補正された最大HPの再計算
+		_correctedMaxHitPoint = new CorrectedMaxHitPoint(_originalMaxHitPoint, _accessory, _armor);
+	}
+
+	public void equip(Armor armor){
+		_armor = armor;
+		// 補正された最大HPの再計算
+		_correctedMaxHitPoint = new CorrectedMaxHitPoint(_originalMaxHitPoint, _accessory, _armor);
+	}
+
 	public void showAbility(){
+		System.out.println("----- member ability -----");
 		System.out.println(" OriginalMaxHitPoint = " + _originalMaxHitPoint._value );
 		System.out.println("Accessory Increments = " + _accessory.maxHitPointIncrements() );
 		System.out.println("    Armor Increments = " + _armor.maxHitPointIncrements() );
 		System.out.println("CorrectedMaxHitPoint = " + _correctedMaxHitPoint._value );
+		System.out.println("--------------------------");
 	}
 }
