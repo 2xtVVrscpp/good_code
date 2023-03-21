6da5bd0e560c245c217a7a102ada5b8f29eb6ad8
sakaaya <space.star.1016.f@gmail.com>
2023-03-21 20:38:21 +0900

    add Location

--- src/java/section12/Main.java
+++ src/java/section12/Main.java
@@ -31,7 +31,7 @@ public class Main {
 			Price defaultPrice1 = new Price(defaultAmount);
 			Price addPrice1 = new Price(defaultAmount);
 			int afterPrice1 = defaultPrice1.badAdd(addPrice1);
-			System.out.println("[bad] after adding Price is " + afterPrice1);
+			System.out.println("[bad ] after adding Price is " + afterPrice1);
 
 			Price defaultPrice2 = new Price(defaultAmount);
 			Price addPrice2 = new Price(defaultAmount);
@@ -39,7 +39,27 @@ public class Main {
 			System.out.println("[good] after adding Price is " + afterPrice2.amount);
 
 			Price defaultPrice3 = new Price(-1000); // throw exception
+			defaultPrice3.goodAdd(new Price(10000)); // not processing.
 		} catch (Exception exception) {
+			System.out.println(exception.getMessage());
+		}
+		System.out.println("================================");
+
+		// *************************************
+		// Not use DoubleMeaning
+		// *************************************
+		System.out.println("==== Not use DoubleMeaning =====");
+		try {
+			int initialX = 10;
+			int initialY = 20;
+			Location location = new Location(initialX, initialY);
+			System.out.println("before (x, y) = (" + location.x + ", " + location.y + ")");
+			Location afterLocation = location.shift(20, -10);
+			System.out.println("after  (x, y) = (" + afterLocation.x + ", " + afterLocation.y + ")");
+
+			Location errorLocation = location.shift(-10, -100); // throw exception
+			System.out.println("after (x, y) = (" + errorLocation.x + ", " + errorLocation.y + ")");// not processing.
+		} catch (IllegalArgumentException exception) {
 			System.out.println(exception.getMessage());
 		}
 		System.out.println("================================");
