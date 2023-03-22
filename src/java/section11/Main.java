e46761d2d14c5e09d51e9816bd3b745ae4cf468e
sakaaya <space.star.1016.f@gmail.com>
2023-03-22 21:51:30 +0900

    fixed section11's value name

--- src/java/section11/Main.java
+++ src/java/section11/Main.java
@@ -11,10 +11,10 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Currency _yen = Currency.getInstance(Locale.JAPAN);
-		Money _money1 = new Money(1000, _yen);
-		Money _money2 = new Money(100, _yen);
-		Money _moneyFinaly = _money1.add(_money2);
-		System.out.println(_moneyFinaly.amount);
+		Currency yen = Currency.getInstance(Locale.JAPAN);
+		Money money1 = new Money(1000, yen);
+		Money money2 = new Money(100, yen);
+		Money moneyFinaly = money1.add(money2);
+		System.out.println(moneyFinaly.amount);
 	}
 }
