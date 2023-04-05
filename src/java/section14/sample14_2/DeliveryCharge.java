936c1ce63c4a2edab881f2bb6443fb2ceb8874f9
shikita-hisato <q238033h@mail.kyutech.jp>
2023-04-05 20:13:30 +0900

    section14 テストコードを用いたリファクタリング DeliveryManager.deliveryChargeメソッドをDeliveryChargeクラスへコピー

--- src/java/section14/sample14_2/DeliveryCharge.java
+++ src/java/section14/sample14_2/DeliveryCharge.java
@@ -3,7 +3,16 @@ package section14.sample14_2;
 public class DeliveryCharge {
 	final int amount;
 
+	/**
+	 * 配送料を返す。
+	 * @param products 配送対象の商品リスト
+	 * @return 配送料
+	 */
 	DeliveryCharge(final ShoppingCart shoppingCart) {
-		amount = DeliveryManager.deliveryCharge(shoppingCart);
+		if(shoppingCart.totalPrice() < 2000) {
+			amount = 500;
+		} else {
+			amount = 0;
+		}
 	}
 }
